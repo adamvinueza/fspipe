4cf74720933899a6ff6d8fa1cc3c370cd56dd4c4
adamvinueza <adamvinueza@pm.me>
2020-09-22 20:23:18 -0600

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fspipe',
-    version='0.6.0',
+    version='0.1.0',
     description='Library for transforming data via streaming files',
     url='http://github.com/adamvinueza/fspipe',
     author='Adam Vinueza',
@@ -11,7 +11,8 @@ setup(
     packages=['fspipe'],
     install_requires=[
         'fsspec==0.8.0',
-        'mypy-extensions==0.4.3', 's3fs==0.5.1',
+        'mypy-extensions==0.4.3',
+        's3fs==0.5.1',
     ],
     zip_safe=False,
 )
